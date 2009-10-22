52d7dd222749b4388c6f8e8817d2a35ac4bbbff8
Antonello Provenzano <antonello@deveel.com>
2009-10-22 03:19:30 +0000

    git-svn-id: http://deveeldb.googlecode.com/svn/trunk@73 de0d096c-ad4d-11de-a70b-4539db072d2e

---
+++ deveeldb-nunit/Deveel.Data/TObjectTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Deveel.Data {
+	[TestFixture]
+	public sealed class TObjectTests {
+		[Test]
+		public void IntegerEqual() {
+			TObject obj1 = 32;
+			TObject obj2 = 21;
+			Assert.IsTrue(obj1 != obj2);
+
+			obj1 = 21;
+			obj2 = 21;
+			Assert.IsTrue(obj1 == obj2);
+		}
+
+		[Test]
+		public void StringEqual() {
+			TObject obj1 = "test1";
+			TObject obj2 = "test2";
+			Assert.IsTrue(obj1 != obj2);
+
+			obj1 = "test_a";
+			obj2 = "test_a";
+			Assert.IsTrue(obj1 != null);
+			Assert.IsTrue(obj1 == obj2);
+
+			obj2 = new TObject(new TStringType(SQLTypes.CHAR, 20, "enUS"), "test_b");
+			Assert.IsTrue(obj1 != obj2);
+		}
+
+		[Test]
+		public void BigNumberEqual() {
+			
+		}
+	}
+}
