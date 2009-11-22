bbdba75c6790158dba2287ebcb3557dc0b5db772
Antonello Provenzano <antonello@deveel.com>
2009-11-22 16:14:48 +0000

    git-svn-id: http://deveeldb.googlecode.com/svn/trunk@123 de0d096c-ad4d-11de-a70b-4539db072d2e

--- deveelsb-shell/Deveel.Data.Commands/CommitCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-using Deveel.Commands;
-
-namespace Deveel.Data.Commands {
-	[Command("commit", RequiresContext = true, ShortDescription = "commits the current transaction")]
-	internal class CommitCommand : SqlCommand {
-	}
-}
