c41a277dc0dcc8b5acb12708ce966021eb25a809
Antonello Provenzano <antonello@deveel.com>
2015-10-02 21:49:23 +0200

    - New tests for creating and granting users - Fixed compilation issue on the configuration of architecture model of the libraries

--- src/deveeldb-nunit/Deveel.Data.Sql/CreateTableTests.cs
+++ src/deveeldb-nunit/Deveel.Data.Sql/CreateTableTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using Deveel.Data;
+using Deveel.Data.Protocol;
+using Deveel.Data.Security;
 using Deveel.Data.Types;
 
 using NUnit.Framework;
@@ -8,6 +10,22 @@ using NUnit.Framework;
 namespace Deveel.Data.Sql {
 	[TestFixture]
 	public class CreateTableTests : ContextBasedTest {
+		private const string TestUserName = "test";
+		private const string TestPassword = "abc1234";
+
+		protected override IQueryContext CreateQueryContext(IDatabase database) {
+			var queryContext = base.CreateQueryContext(database);
+
+			if (TestContext.CurrentContext.Test.Name.Equals("CreateSimple_RegularUser")) {
+				var user = queryContext.CreateUser(TestUserName, TestPassword);
+				queryContext.GrantHostAccessToUser(TestUserName, KnownConnectionProtocols.Local, "%");
+				queryContext.GrantToUserOnSchemaTables("APP", user, User.System, Privileges.Create);
+				queryContext.Session.Commit();
+			}
+
+			return queryContext;
+		}
+
 		[Test]
 		public void CreateSimpleTableAsAdmin_NewSession() {
 			var tableName = ObjectName.Parse("APP.test_table");
@@ -17,9 +35,7 @@ namespace Deveel.Data.Sql {
 
 			using (var session = Database.CreateUserSession(AdminUserName, AdminPassword)) {
 				using (var context = new SessionQueryContext(session)) {
-					Assert.DoesNotThrow(() => {
-						context.CreateTable(tableInfo);
-					});
+					Assert.DoesNotThrow(() => context.CreateTable(tableInfo));
 				}
 
 				Assert.DoesNotThrow(() => session.Commit());
@@ -31,6 +47,22 @@ namespace Deveel.Data.Sql {
 					Assert.DoesNotThrow(() => exists = context.TableExists(tableName));
 					Assert.IsTrue(exists);
 				}
+			}
+		}
+
+		[Test]
+		public void CreateSimple_RegularUser() {
+			var tableName = ObjectName.Parse("APP.test_table");
+			var tableInfo = new TableInfo(tableName);
+			tableInfo.AddColumn("a", PrimitiveTypes.Integer(), true);
+			tableInfo.AddColumn("b", PrimitiveTypes.String());
+
+			using (var session = Database.CreateUserSession(TestUserName, TestPassword)) {
+				using (var context = new SessionQueryContext(session)) {
+					Assert.DoesNotThrow(() => context.CreateTable(tableInfo));
+				}
+
+				Assert.DoesNotThrow(() => session.Commit());
 			}
 		}
 	}
---
+++ src/deveeldb-nunit/Deveel.Data.Sql/CreateUserTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Deveel.Data.Protocol;
+using Deveel.Data.Security;
+
+using NUnit.Framework;
+
+namespace Deveel.Data.Sql {
+	[TestFixture]
+	public sealed class CreateUserTests : ContextBasedTest {
+		[Test]
+		public void Create() {
+			User user = null;
+
+		 	Assert.DoesNotThrow(() => user = QueryContext.CreateUser("test", "abc1234"));
+			Assert.IsNotNull(user);
+
+			Assert.DoesNotThrow(() => QueryContext.Session.Commit());
+
+			bool userExists = false;
+			Assert.DoesNotThrow(() => userExists = QueryContext.UserExists("test"));
+			Assert.IsTrue(userExists);
+		}
+
+		[Test]
+		public void CreateAndAuthenticate() {
+			User user = null;
+
+			Assert.DoesNotThrow(() => user = QueryContext.CreateUser("test", "abc1234"));
+
+			// We need to grant access to local if we want to authenticate
+			Assert.DoesNotThrow(() => QueryContext.GrantHostAccessToUser("test", KnownConnectionProtocols.Local, "%"));
+			Assert.IsNotNull(user);
+
+			Assert.AreEqual("test", user.Name);
+
+			Assert.DoesNotThrow(() => QueryContext.Session.Commit());
+
+			Assert.DoesNotThrow(() => user = Database.Authenticate("test", "abc1234"));
+			Assert.IsNotNull(user);
+			Assert.AreEqual("test", user.Name);
+		}
+	}
+}
