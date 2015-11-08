0a28729b55f07dd0d81798886895c31a4d5644d9
Antonello Provenzano <antonello@deveel.com>
2015-11-08 01:56:39 +0100

    De-reference of the external projects from the core NUnit test suite

--- src/deveeldb-nunit/Deveel.Data.Types/DataTypeParseTests.cs
+++ src/deveeldb-nunit/Deveel.Data.Types/DataTypeParseTests.cs
@@ -1,26 +1,10 @@
 ﻿using System;
 
-using Deveel.Data;
-using Deveel.Data.Spatial;
-
 using NUnit.Framework;
 
 namespace Deveel.Data.Types {
 	[TestFixture]
 	public sealed class DataTypeParseTests : ContextBasedTest {
-		protected override ISystemContext CreateSystemContext() {
-			var context = base.CreateSystemContext();
-			context.UserSpatial();
-			return context;
-		}
-
-		[Test]
-		public void SimpleGeometryWithNoContext() {
-			const string typeString = "GEOMETRY";
-
-			Assert.Throws<NotSupportedException>(() =>  SqlType.Parse(typeString));
-		}
-
 		[Test]
 		public void SimpleNumeric() {
 			const string typeString = "NUMERIC";
@@ -30,29 +14,6 @@ namespace Deveel.Data.Types {
 			Assert.IsNotNull(type);
 			Assert.IsInstanceOf<NumericType>(type);
 			Assert.AreEqual(-1, ((NumericType)type).Size);
-		}
-
-
-		[Test]
-		public void SimpleGeometry() {
-			const string typeString = "GEOMETRY";
-
-			SqlType type = null;
-			Assert.DoesNotThrow(() => type = SqlType.Parse(QueryContext, typeString));
-			Assert.IsNotNull(type);
-			Assert.IsInstanceOf<SpatialType>(type);
-			Assert.AreEqual(-1, ((SpatialType)type).Srid);
-		}
-
-		[Test]
-		public void GeometryWithSrid() {
-			const string typeString = "GEOMETRY(SRID = '2029')";
-
-			SqlType type = null;
-			Assert.DoesNotThrow(() => type = SqlType.Parse(QueryContext, typeString));
-			Assert.IsNotNull(type);
-			Assert.IsInstanceOf<SpatialType>(type);
-			Assert.AreEqual(2029, ((SpatialType)type).Srid);
 		}
 	}
 }
---
+++ src/deveeldb.gis.nunit/Deveel.Data.Types/DataTypeParseTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Deveel.Data.Spatial;
+
+using NUnit.Framework;
+
+namespace Deveel.Data.Types {
+	[TestFixture]
+	public sealed class DataTypeParseTests : ContextBasedTest {
+		protected override ISystemContext CreateSystemContext() {
+			var context = base.CreateSystemContext();
+			context.UserSpatial();
+			return context;
+		}
+
+		[Test]
+		public void SimpleGeometryWithNoContext() {
+			const string typeString = "GEOMETRY";
+
+			Assert.Throws<NotSupportedException>(() => SqlType.Parse(typeString));
+		}
+
+		[Test]
+		public void SimpleGeometry() {
+			const string typeString = "GEOMETRY";
+
+			SqlType type = null;
+			Assert.DoesNotThrow(() => type = SqlType.Parse(QueryContext, typeString));
+			Assert.IsNotNull(type);
+			Assert.IsInstanceOf<SpatialType>(type);
+			Assert.AreEqual(-1, ((SpatialType)type).Srid);
+		}
+
+		[Test]
+		public void GeometryWithSrid() {
+			const string typeString = "GEOMETRY(SRID = '2029')";
+
+			SqlType type = null;
+			Assert.DoesNotThrow(() => type = SqlType.Parse(QueryContext, typeString));
+			Assert.IsNotNull(type);
+			Assert.IsInstanceOf<SpatialType>(type);
+			Assert.AreEqual(2029, ((SpatialType)type).Srid);
+		}
+	}
+}
