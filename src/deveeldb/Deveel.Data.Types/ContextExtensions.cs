4e0ca36ea1ea67e681c034c4d39b09d3d6ec96ee
Antonello Provenzano <antonello@deveel.com>
2015-11-22 04:59:04 +0100

    Type resolving failing fixed

--- src/deveeldb/Deveel.Data.Types/ContextExtensions.cs
+++ src/deveeldb/Deveel.Data.Types/ContextExtensions.cs
@@ -5,7 +5,17 @@ using Deveel.Data.Services;
 namespace Deveel.Data.Types {
 	public static class ContextExtensions {
 		public static SqlType ResolveType(this IContext context, string typeName, params DataTypeMeta[] meta) {
-			throw new NotImplementedException();
+			if (PrimitiveTypes.IsPrimitive(typeName))
+				return PrimitiveTypes.Resolve(typeName, meta);
+
+			var resolvers = context.ResolveAllServices<ITypeResolver>();
+			foreach (var resolver in resolvers) {
+				var sqlType = resolver.ResolveType(new TypeResolveContext(SqlTypeCode.Unknown, typeName, meta));
+				if (sqlType != null)
+					return sqlType;
+			}
+
+			return null;
 		}
 
 		public static ITypeResolver TypeResolver(this IContext context) {
