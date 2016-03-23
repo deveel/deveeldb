b992251c739369a759c732d048e4b121dcf83d64
Antonello Provenzano <antonello@deveel.com>
2016-03-23 15:41:38 +0100

    Implementation of the SQL syntax NEXT VALUE FOR (to support feature request #120 )

---
+++ src/deveeldb-nunit/Deveel.Data.Sql.Compile/NextValueForTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+using Deveel.Data.Sql.Statements;
+
+using NUnit.Framework;
+
+namespace Deveel.Data.Sql.Compile {
+	[TestFixture]
+	public sealed class NextValueForTests : SqlCompileTestBase {
+		[Test]
+		public void Simple() {
+			const string sql = "NEXT VALUE FOR app.seq1";
+
+			var result = Compile(sql);
+
+			Assert.IsNotNull(result);
+			Assert.IsFalse(result.HasErrors);
+
+			Assert.AreEqual(1, result.Statements.Count);
+
+			var statement = result.Statements.ElementAt(0);
+
+			Assert.IsInstanceOf<SelectStatement>(statement);
+		}
+	}
+}
---
+++ src/deveeldb/Deveel.Data.Sql.Parser/NextSequenceValueNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Deveel.Data.Sql.Expressions;
+using Deveel.Data.Sql.Statements;
+
+namespace Deveel.Data.Sql.Parser {
+	class NextSequenceValueNode : SqlStatementNode {
+		public ObjectNameNode SequenceName { get; private set; }
+
+		protected override ISqlNode OnChildNode(ISqlNode node) {
+			if (node is ObjectNameNode)
+				SequenceName = (ObjectNameNode) node;
+
+			return base.OnChildNode(node);
+		}
+
+		protected override void BuildStatement(SqlStatementBuilder builder) {
+			var funcRef = SqlExpression.FunctionCall("NEXTVALUE", new[] {SqlExpression.Constant(SequenceName.Name)});
+			var query = new SqlQueryExpression(new [] {new SelectColumn(funcRef) });
+			var select = new SelectStatement(query);
+
+			builder.AddObject(select);
+		}
+	}
+}
