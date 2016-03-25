321bc95ec56ba9344db0dc0cc855f79d362f9894
Antonello Provenzano <antonello@deveel.com>
2016-03-25 23:26:50 +0100

    Made SqlCompileTestBase abstract

--- src/deveeldb-nunit/Deveel.Data.Sql.Compile/SqlCompileTestBase.cs
+++ src/deveeldb-nunit/Deveel.Data.Sql.Compile/SqlCompileTestBase.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 
 namespace Deveel.Data.Sql.Compile {
 	[TestFixture]
-	public class SqlCompileTestBase {
+	public abstract class SqlCompileTestBase {
 		protected SqlCompileResult Compile(string sql) {
 			var compiler=new SqlDefaultCompiler();
 			return compiler.Compile(new SqlCompileContext(sql));
