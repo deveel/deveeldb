da6ac8efe33cef8786181f37b777b7ff4efa4a9e
Antonello Provenzano <antonello@deveel.com>
2016-03-29 08:53:38 +0200

    Updates of the license headers in source files

--- src/deveeldb/Deveel.Data.Sql.Objects/SqlTabular.cs
+++ src/deveeldb/Deveel.Data.Sql.Objects/SqlTabular.cs
@@ -1,5 +1,5 @@
 ﻿// 
-//  Copyright 2010-2015 Deveel
+//  Copyright 2010-2016 Deveel
 // 
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 //
+
 
 using System;
 using System.Collections;
