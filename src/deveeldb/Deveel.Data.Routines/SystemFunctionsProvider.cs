aa6d41a999054f87425b6abfed76c0d2969443f1
Antonello Provenzano <antonello@deveel.com>
2016-07-21 08:47:26 +0200

    NEW_OBJECT function return type

--- src/deveeldb/Deveel.Data.Routines/SystemFunctionsProvider.cs
+++ src/deveeldb/Deveel.Data.Routines/SystemFunctionsProvider.cs
@@ -504,13 +504,16 @@ namespace Deveel.Data.Routines {
 				.WhenExecute(context => {
 					var typeName = context.EvaluatedArguments[0];
 					var argc = context.ArgumentCount;
-					var args = new Field[argc-1];
+					var args = new Field[argc - 1];
 					if (argc > 1) {
 						Array.Copy(context.EvaluatedArguments, 1, args, 0, args.Length);
 					}
 
 					var obj = SystemFunctions.NewObject(context.Request, typeName, args);
 					return context.Result(obj);
+				}).ReturnsType(context => {
+					var typeName = context.EvaluatedArguments[0].Value.ToString();
+					return context.Request.Access().ResolveUserType(typeName);
 				}));
 		}
 
