dfdc5b59da959eb43002082b53c084d2f6f27cd2
Antonello Provenzano <antonello@deveel.com>
2016-09-17 18:33:51 +0200

    Inclusion of missing files from merge

---
+++ src/deveeldb/Deveel.Data.Mapping/ConstraintAttribute.cs
@@ -0,0 +1,36 @@
+﻿// 
+//  Copyright 2010-2016 Deveel
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+
+using System;
+
+using Deveel.Data.Sql.Tables;
+
+namespace Deveel.Data.Mapping {
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public sealed class ConstraintAttribute : Attribute {
+		public ConstraintAttribute(ConstraintType type) {
+			if (type == ConstraintType.ForeignKey)
+				throw new NotImplementedException();
+
+			Type = type;
+		}
+
+		public ConstraintType Type { get; private set; }
+
+		public string Expression { get; set; }
+	}
+}
