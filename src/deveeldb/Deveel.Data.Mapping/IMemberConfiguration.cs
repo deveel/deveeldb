826854e821654d31bb59b181c2dd3d75c255c66a
Antonello Provenzano <antonello@deveel.com>
2016-09-19 09:50:07 +0200

    More advanced configuration of entities (staged: passing to another machine)

---
+++ src/deveeldb/Deveel.Data.Mapping/IMemberConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Deveel.Data.Sql.Expressions;
+using Deveel.Data.Sql.Types;
+
+namespace Deveel.Data.Mapping {
+	public interface IMemberConfiguration {
+		IMemberConfiguration HasColumnName(string value);
+
+		IMemberConfiguration HasColumnType(SqlType value);
+
+		IMemberConfiguration NotNull(bool value = true);
+
+		IMemberConfiguration PrimaryKey(bool value = true);
+
+		IMemberConfiguration Unique(bool value = true);
+
+		IMemberConfiguration HasDefault(SqlExpression defaultExpression);
+
+		IMemberConfiguration Ignore(bool value = true);
+	}
+}
