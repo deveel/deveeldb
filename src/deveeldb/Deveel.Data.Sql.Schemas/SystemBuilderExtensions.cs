c3682ec781b3f4356d13441e741d7669aee50e4e
Antonello Provenzano <antonello@deveel.com>
2016-11-03 12:25:53 +0100

    Applied license headers

--- src/deveeldb/Deveel.Data.Sql.Schemas/SystemBuilderExtensions.cs
+++ src/deveeldb/Deveel.Data.Sql.Schemas/SystemBuilderExtensions.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿// 
+//  Copyright 2010-2016 Deveel
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+
+using System;
 
 using Deveel.Data.Build;
-using Deveel.Data.Security;
-using Deveel.Data.Sql.Expressions;
-using Deveel.Data.Sql.Expressions.Build;
 using Deveel.Data.Sql.Tables;
 using Deveel.Data.Sql.Types;
 
@@ -30,15 +44,6 @@ namespace Deveel.Data.Sql.Schemas {
 				.WithColumn("type", PrimitiveTypes.String())
 				.WithColumn("culture", PrimitiveTypes.String())
 				.WithColumn("other", PrimitiveTypes.String()));
-
-			//var tableInfo = new TableInfo(SystemSchema.SchemaInfoTableName);
-			//tableInfo.AddColumn("id", PrimitiveTypes.Numeric());
-			//tableInfo.AddColumn("name", PrimitiveTypes.String());
-			//tableInfo.AddColumn("type", PrimitiveTypes.String());
-			//tableInfo.AddColumn("culture", PrimitiveTypes.String());
-			//tableInfo.AddColumn("other", PrimitiveTypes.String());
-			//tableInfo = tableInfo.AsReadOnly();
-			//systemQuery.Access().CreateTable(tableInfo);
 
 			// TODO: Move this to the setup phase?
 			CreateSystemSchema(systemQuery);
--- src/deveeldb/Deveel.Data.Sql.Statements.Build/ColumnBuilderExtensions.cs
+++ src/deveeldb/Deveel.Data.Sql.Statements.Build/ColumnBuilderExtensions.cs
@@ -1,9 +1,38 @@
-﻿using System;
+﻿// 
+//  Copyright 2010-2016 Deveel
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
 
+
+using System;
+
+using Deveel.Data.Sql.Expressions.Build;
 using Deveel.Data.Sql.Tables;
 
 namespace Deveel.Data.Sql.Statements.Build {
 	public static class ColumnBuilderExtensions {
+		public static IColumnBuilder WithDefault(this IColumnBuilder builder, Action<IExpressionBuilder> expression) {
+			var expBuilder = new ExpressionBuilder();
+			expression(expBuilder);
+
+			return builder.WithDefault(expBuilder.Build());
+		}
+
+		public static IColumnBuilder WithDefault(this IColumnBuilder builder, object value) {
+			return builder.WithDefault(exp => exp.Value(value));
+		}
+
 		public static IColumnBuilder PrimaryKey(this IColumnBuilder builder) {
 			return builder.WithConstraint(new ColumnConstraintInfo(ConstraintType.PrimaryKey));
 		}
